c768239c7cdf26eebabaee09eb1059d63381b131
Bhavishm009 <bhavishm009@gmail.com>
2023-02-25 00:29:13 +0530

    change's mfes configed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/views/Header/Header";
-import LoginPage from "./components/views/LoginPage/LoginPage";
 import Navbar from "./components/views/Navbar/Navbar";
 import Hero from './components/views/Hero/Hero'
-import { ProductCard } from "./components/views/ProductCard/ProductCard";
 import "./index.css";
-import Text from './components/views/Text/Text'
-import RegisterPage from "./components/views/RegisterPage/RegisterPage";
-import Banner4 from "./components/views/Banner4/Banner4";
-import MyButton from './components/views/MyButton/MyButton'
-import { productData } from './Constdata/mockdata'
+// import LoginPage from 'LoginPageMFE/LoginPage'
 const App = () => (
   <div >
     <Header />
     <Navbar />
-     <Hero />
+   <Hero/>
+   {/* <LoginPage/> */}
     {/* <Banner4>
     <button>Shop Now</button>
    </Banner4> */}
